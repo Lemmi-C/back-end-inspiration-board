4b13c4b84abb7c9c6f66a452233a20e75bae13df
Ana Silvera <anasilverx@gmail.com>
2023-06-26 14:49:51 -0700

    create Blueprint for boards routes. implement POST method. register blueprint on app

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,28 @@
 from flask import Blueprint, request, jsonify, make_response
 from app import db
+from app.models.board import Board
 
 # example_bp = Blueprint('example_bp', __name__)
+boards_bp = Blueprint("boards", __name__, url_prefix="/boards")
+
+@boards_bp.route("", methods=['GET'])
+def handle_boards():
+    pass
+
+@boards_bp.route("", methods=['POST'])
+def create_board():
+    request_body = request.get_json()
+
+    if 'title' not in request_body or 'owner' not in request_body:
+        return {"error": "Must provide title and owner"}
+
+    new_board = Board(title=request_body['title'], owner=request_body['owner'])
+
+    db.session.add(new_board)
+    db.session.commit()
+
+    return {
+        "board_id": new_board.board_id,
+        "title": new_board.title,
+        "owner": new_board.owner
+    }, 201
